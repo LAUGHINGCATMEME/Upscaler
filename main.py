f43eb4620620530fc74a2d59fba8adf9b52f3506
Laughing Cat Meme <aumshree.personal@gmail.com>
2023-06-02 12:23:14 +0530

    Final commit for now

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
 
 current_upscale = ""
+failed_images = {}
 
 
 def convert_to_png(image_path):
@@ -152,6 +153,49 @@ def upscale_folder(input_folder_path, output_folder_path):
         print(f"Input/Output folder doesnt exists.")
 
 
+def get_image_dimensions(image_path):
+    try:
+        with Image.open(image_path) as img:
+            width, height = img.size
+            return width, height
+    except IOError:
+        print("Unable to open image file:", image_path)
+        return None
+
+
+def split_image(image_path, original_width, original_height, part_width, part_height):
+    try:
+        with Image.open(image_path) as img:
+            for i in range(int(original_width / part_width)):
+                for j in range(int(original_height / part_height)):
+                    left = j * part_width
+                    upper = i * part_height
+                    right = left + part_width
+                    lower = upper + part_height
+
+                    part_img = img.crop((left, upper, right, lower))
+                    part_img.save(f"C2\\part_{i}_{j}.png")  # Change the filename and extension as needed
+
+            print(f"Image split into {int(original_width / part_width) * int(original_height / part_height)} parts successfully.")
+    except IOError:
+        print("Unable to open image file:", image_path)
+
+
+def combine_images(original_width, original_height, part_width, part_height):
+    combined_width = original_width
+    combined_height = original_height
+    combined_image = Image.new("RGB", (combined_width, combined_height))
+
+    for i in range(int(original_width / part_width)):
+        for j in range(int(original_height / part_height)):
+            part_filename = f"part_{i}_{j}.png"  # Adjust the filename format if needed
+            part_image = Image.open(part_filename)
+            combined_image.paste(part_image, (j * part_width, i * part_height))
+
+    combined_image.save("BROOOOOOOOOO.png")  # Change the filename and extension as needed
+    print("Images combined into one.")
+
+
 def invalid_input(inp):
     """
     Prints an error message for invalid input and exits the program.
@@ -195,5 +239,5 @@ if __name__ == '__main__':
     else:
         upscale_image(args.input, args.output) if args.type_of_upscale == "1" else upscale_folder(args.input, args.output)
         """
-
+    
 
